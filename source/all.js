6f68fe52218a9f9b44fc6cb731b89a3c20f98f07
Michael Lajlev <lajlev@gmail.com>
2014-01-06 15:19:07 +0100

    first version

---
+++ source/all.js
@@ -0,0 +1,5 @@
+function gen_mail_to_link(lhs,rhs,subject) {
+  document.write("<A HREF=\"mailto");
+  document.write(":" + lhs + "@");
+  document.write(rhs + "?subject=" + subject + "\">" + lhs + "@" + rhs + "<\/A>");
+}
